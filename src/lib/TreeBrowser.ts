9986e94f692685b04adb045f9c810ebd04a8806c
Dave Goodchild <dave@davegoodchild.co.uk>
2017-05-24 10:26:08 +0200

    Upgrade tree to support indeterminate states and togglesParent option. Improved the build settings. Updated the example to show more options

--- src/lib/TreeBrowser.ts
+++ src/lib/TreeBrowser.ts
@@ -1,13 +1,13 @@
 interface NodeConfigCheckbox {
 	enabled: boolean, // if false, everything else is ignored.
-	name: string,
-	value: string,
+	name?: string,
+	value?: string,
 	// todo: radio: boolean // allows setting radio so that any other sibling that is radio also is toggled.
-	pseudo: boolean, // means don't send to backend
+	pseudo?: boolean, // means don't send to backend
 	// todo: triState: boolean, // if true it uses a box to represent selected + some children
-	state: number, //-1, 0, 1 //(-1=semi) or maybe an enum?
+	state?: number, //-1, 0, 1 //(-1=semi) or maybe an enum?
 	// todo: clickable: boolean, // could make it look faint if false, i.e. is passive
-	togglesChildren: boolean,
+	togglesChildren?: boolean,
 	// todo: togglesParent: boolean,
 }
 
@@ -17,23 +17,23 @@ interface NodeConfigIcon {
 
 interface NodeConfigExpand {
 	enabled: boolean,
-	expanded: boolean
+	expanded?: boolean
 }
 
 interface NodeConfig {
 	text: string,
 	checkbox: NodeConfigCheckbox,
-	icon: NodeConfigIcon,
+	//icon: NodeConfigIcon,
 	expand: NodeConfigExpand
 	children: NodeConfig[]
 }
 
 interface TreeBrowserConfig {
 	nodes: NodeConfig[],
-	displayRoots: boolean,
+	displayRoots?: boolean,
 	jquery: string, // some people don't allow the global "$", so needs to be configurable
 	renderTo: JQuery,
-	renderDelegates: any // todo: interface with supported delegates
+	renderDelegates?: any // todo: interface with supported delegates
 }
 
 interface Window { [key: string]: any }
@@ -48,26 +48,34 @@ export default class TreeBrowser {
 	}
 
 	private init() {
-		const _this = this;
+		const rootNodes = this.config.nodes.map( _ => this.generateNodeHtml(_) );
 
-		const rootNodes = this.config.nodes.map( _ => _this.generateNodeHtml(_) );
-
-		this.config.renderTo.append( this.query( '<ul>' ).append( rootNodes ) );
+		this.config.renderTo
+			.append( this.query( '<ul>' ).append( rootNodes ) )
+			.addClass( 'tree-browser' );
 
 		this.query( 'a.toggle', this.config.renderTo )
-			.on( 'click.tree-browser', ( e: any ) => _this.onClickExpandToggle( e ) );
-		this.query( 'input[type="checkbox"]', this.config.renderTo )
-			.on( 'change.tree-browser', ( e: any ) => _this.onChangeCheckbox( e ) );
+			.on( 'click.tree-browser', ( e: any ) => this.onClickExpandToggle( e ) );
 
-		// todo: full row select
+		this.query( 'input[type="checkbox"]', this.config.renderTo )
+			.on( 'change.tree-browser', ( e: any ) => this.onChangeCheckbox( e ) );
+
+		this.query( '.treenode-content > a:last-child', this.config.renderTo )
+			.on( 'click.tree-browser', ( e: any ) => this.onClickRow( e ) );
 	}
 
 	private applyConfigDefaults() {
 		this.config.jquery = this.config.jquery || '$';
 	}
 
-	private applyNodeDefaults() {
-
+	private applyNodeDefaults( nodeConfig: any ) {
+		const defaultConfig = {
+			text: '',
+			checkbox: { enabled: false },
+			expand: { enabled: false },
+			children: []
+		};
+		return $.extend( {}, defaultConfig, nodeConfig );
 	}
 
 	private query( anything: any, parentEl: JQuery = null ): JQuery {
@@ -76,18 +84,34 @@ export default class TreeBrowser {
 
 	private onChangeCheckbox( event: any ) {
 		const checkbox = this.query( event.currentTarget );
+		const isChecked = checkbox.prop( 'checked' );
+		const isHandlerOnly = event.handlerOnly || false;
+
 		const treeNodeLi = checkbox.closest( 'li' );
 		const node = treeNodeLi.data( 'treenode' );
 
 		if ( node.checkbox.togglesParent ) {
-			const parentUl = treeNodeLi.parent();
-			//parentUl.find( 'input[type="checkbox"]' )
+			const parentUl = treeNodeLi.closest( 'ul' );
+			const siblingCheckboxes = parentUl.find( '> li > .treenode-content input[type="checkbox"]' );
+			const checkedSiblings = siblingCheckboxes.filter( ( i:number, el: HTMLElement ) => this.query( el ).is( ':checked' ) );
+			const indeterminateSiblings = siblingCheckboxes.filter( ( i:number, el: HTMLElement ) => this.query( el ).is( ':indeterminate' ) );
+
+			const isSomeChecked = ( checkedSiblings.length > 0 && checkedSiblings.length != siblingCheckboxes.length );
+			const isNoneChecked = ( checkedSiblings.length == 0 );
+			const isAllChecked = !isNoneChecked && !isSomeChecked;
+			const isSomeIndeterminate = ( indeterminateSiblings.length > 0 );
+
+			const parentCheckbox = parentUl.siblings( '.treenode-content' ).find( 'input[type="checkbox"]' );
+
+			parentCheckbox.prop( 'indeterminate', isSomeChecked || isSomeIndeterminate );
+			parentCheckbox.prop( 'checked', isAllChecked );
+
+			parentCheckbox.triggerHandler( { type: "change", handlerOnly: true } );
 		}
 
-		if ( node.checkbox.togglesChildren ) {
+		if ( !isHandlerOnly && node.checkbox.togglesChildren ) {
 			treeNodeLi.find( 'ul input[type=checkbox]' ).prop( 'checked', checkbox.prop( 'checked' ) );
 		}
-		// todo: callback for onAfterChangeCheckbox
 	}
 
 	private onClickExpandToggle( event: any ) {
@@ -99,18 +123,27 @@ export default class TreeBrowser {
 		expandLink.parent().siblings( 'ul' ).toggleClass( 'hidden' );
 	}
 
+	private onClickRow( event: any ) {
+		event.preventDefault();
+		event.stopPropagation();
+
+		this.query( 'input[type="checkbox"]', this.query( event.currentTarget ).parent() ).click();
+	}
+
 	private generateNodeHtml( node: NodeConfig ): JQuery {
+		node = this.applyNodeDefaults( node );
+
 		let nodeHtml: JQuery = this.query( `
 			<li class="treenode">
 				<div class="treenode-content">
 					<a unselectable="on" class="node-title">${node.text}</a>
 				</div>
-				<div class="treenode-overlay"></div>
+				<div class="treenode-underlay"></div>
 				<ul class="hidden"></ul>
 			</li>`
 		);
 
-		const nodeContent = nodeHtml.find( '> div.treenode-content' );
+		const nodeContent = nodeHtml.find( '> .treenode-content' );
 
 		if ( node.checkbox.enabled ) {
 			let checkboxContainer: JQuery;
@@ -124,7 +157,13 @@ export default class TreeBrowser {
 			}
 
 			const checkbox = checkboxContainer.find( 'input[type="checkbox"]' );
-			checkbox.toggleClass( 'pseudo', node.checkbox.pseudo );
+			if ( node.checkbox.pseudo ) {
+				checkbox
+					.addClass( 'pseudo' )
+					.prop( 'pseudo', true )
+					.removeAttr( 'name' )
+					.removeAttr( 'value' );
+			}
 
 			if ( node.checkbox.state == 1 ) {
 				checkbox.attr( 'checked', 'checked' );
